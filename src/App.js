d43303cf1169bfdc01791eb48536fea11b7e767e
Aline Chavez <alinechavez37@gmail.com>
2024-12-17 18:02:12 -0500

    running react application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Aline-react Application</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
