e5f2ad270547cb0de33ca0e27bc82138e534356f
Aline Chavez <alinechavez37@gmail.com>
2025-06-14 12:43:40 -0500

    Added images

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Homepage.css';
+import logo from './logo.png';
+import image from './image.jpg';
 
 const Homepage = () => {
   return (
     <div className="homepage-container">
       <header className="homepage-header">
-        <div className="logo">myPayslip</div>
+        <div className="left-header">
+          <img src={logo} alt="Logo" className="logo-img" />
+          <div className="app-name">myPayslip</div>
+        </div>
         <div className="buttons">
           <button className="login-btn">Iniciar Sesión</button>
           <button className="register-btn">Registrarse</button>
@@ -13,11 +18,11 @@ const Homepage = () => {
       </header>
 
       <main className="homepage-main">
+        <div className="image-section">
+          <img src={image} alt="Ilustración" />
+        </div>
         <div className="text-section">
           <h1>Transformamos<br />números<br />en confianza</h1>
-        </div>
-        <div className="image-section">
-            <img src="./image.jpg" alt="Ilustracion"/>
         </div>
       </main>
     </div>
@@ -25,3 +30,4 @@ const Homepage = () => {
 };
 
 export default Homepage;
+
